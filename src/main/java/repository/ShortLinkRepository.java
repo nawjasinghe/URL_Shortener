0ea78bb376fd719c14aa0d73613992139ef4d67a
Naween Jasinghe <jasinghenaween@gmail.com>
2025-12-28 16:07:19 -0500

    database access layer for talking to the database

---
+++ src/main/java/repository/ShortLinkRepository.java
@@ -0,0 +1,12 @@
+package repository;
+import com.naw.url.model.ShortLink;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ShortLinkRepository extends JpaRepository<ShortLink, Long> {
+    Optional<ShortLink> findByCode(String code);
+    boolean existsByCode(String code);
+}
