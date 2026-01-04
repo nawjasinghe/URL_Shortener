174ebe4be8c09d234d7cb98910e6bc2a140fa125
Naween Jasinghe <jasinghenaween@gmail.com>
2026-01-04 15:46:20 -0500

    Fix formatting issues in requests.java

--- src/main/java/com/naw/url/model/requests.java
+++ src/main/java/com/naw/url/model/requests.java
@@ -3,7 +3,6 @@ package com.naw.url.model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-
 import java.time.LocalDateTime;
 
 public class requests {
