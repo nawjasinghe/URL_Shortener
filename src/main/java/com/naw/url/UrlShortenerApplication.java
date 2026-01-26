c3f9fdc59be2ff963601eacd3fd50e640ca90996
Naween Jasinghe <jasinghenaween@gmail.com>
2026-01-25 19:22:33 -0500

    Remove line in UrlShortenerApplication
    Removed an unnecessary blank line in UrlShortenerApplication.java

--- src/main/java/com/naw/url/UrlShortenerApplication.java
+++ src/main/java/com/naw/url/UrlShortenerApplication.java
@@ -1,7 +1,6 @@
 package com.naw.url;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class UrlShortenerApplication {
 
